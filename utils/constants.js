7eae18bc7eb586fb8cf69315459ebabfffa852e8
Adarsh <adarshbhalotia2910@gmail.com>
2018-11-16 00:50:31 +0530

    Add util

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PUT_PATIENT_IN_STORAGE: 'put_patient_in_storage',
+  HANDLE_PUT_PATIENT_IN_STORAGE: 'handle_put_patient_in_storage',
+
+  GET_PATIENT_FROM_STORAGE: 'get_patient_from_storage',
+  HANDLE_GET_PATIENT_FROM_STORAGE: 'handle_get_patient_from_storage',
+
+
+  CLEAR_STORAGE: 'clear_storage',
+  HANDLE_CLEAR_STORAGE: 'handle_clear_storage',
+};
