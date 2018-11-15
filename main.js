2616fb89ce9e05e37e702291dc32342b82d325c6
Adarsh <adarshbhalotia2910@gmail.com>
2018-11-15 23:49:41 +0530

    Add developer options

--- main.js
+++ main.js
@@ -33,3 +33,21 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+if(process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
