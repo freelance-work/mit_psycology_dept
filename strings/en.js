be1df33bc74f8ef43e0cbf6bc6359d9f6964c8d6
Adarsh <adarshbhalotia2910@gmail.com>
2018-11-22 04:44:42 +0530

    Add kannada and english string for game page

--- strings/en.js
+++ strings/en.js
@@ -3,5 +3,11 @@ module.exports = {
     title: 'Enter patient ID',
     placeholder: 'Patient ID',
     buttonText: 'Start'
-  },
+  },  
+  gamePage: {
+    game1: 'Spot Emotion',
+    game2: 'Face Reaction',
+    game3: 'Word Reaction',
+    game4: 'IOWA Gambling'
+  }
 }
--- strings/kn.js
+++ strings/kn.js
@@ -4,4 +4,10 @@ module.exports = {
     placeholder: 'ರೋಗಿಯ ID',
     buttonText: 'ಪ್ರಾರಂಭಿಸಿ'
   },
+  gamePage: {
+    game1: 'ಭಾವನೆ ಗುರುತಿಸಿ',
+    game2: 'ಮುಖಭಾವ ಪ್ರತಿಕ್ರಿಯೆ',
+    game3: 'ಪದಗಳ ಭಾವನೆ ಪ್ರತಿಕ್ರಿಯೆ',
+    game4: 'IOWA ಜೂಜಾಟ'
+  }
 }
