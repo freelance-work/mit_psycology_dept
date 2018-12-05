5d8c0627a8f3bfa1e64f9e108f3a5f0883affb52
Adarsh <adarshbhalotia2910@gmail.com>
2018-12-06 00:08:46 +0530

    Remove ipcRendered call

--- utils/csvHelper.js
+++ utils/csvHelper.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 const electron =  require('electron');
 const { ipcRenderer } = electron;
 
-const {
-  GET_PATIENT_FROM_STORAGE,
-} = require('./constants');
 
-exports.write = async function(json) {
+exports.write = async function(json, id) {
   try {
     const csv = json2csv(json);
 
-    let patientId;
+    let patientId = id;
 
     let today = new Date();
     let dd = today.getDate();
@@ -27,15 +24,12 @@ exports.write = async function(json) {
     } 
     let date = dd + '-' + mm + '-' + yyyy;
 
-    await ipcRenderer.send(GET_PATIENT_FROM_STORAGE, (e, res) => {
-      if(res.success) {
-        patientId = res.id;
-      }
-    });
-
-    fs.writeFile(patientId+date+'.csv', csv, function(err, data){
+    fs.writeFile('emo_recognition_'+patientId+'_'+date+'.csv', csv, function(err, data){
       if (err) console.log(err);
       console.log("Successfully Written to File.");
     });
+  } catch(err) {
+    console.log("Couldn't export to CSV");
+    return "failed";
   }
 }
