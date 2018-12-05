bcd5c394e57a6ac9b7d0d1e3392d20bee4fd973b
Adarsh <adarshbhalotia2910@gmail.com>
2018-12-05 22:26:40 +0530

    Add helper functions to store data from task 1

--- utils/constants.js
+++ utils/constants.js
@@ -10,4 +10,7 @@ module.exports = {
   HANDLE_CLEAR_STORAGE: 'handle_clear_storage',
 
   HANDLE_LANGUAGE_CHANGE: 'handle_language_change',
+  
+  PUT_EMOTION_RECOGNITION_DATA: 'put_emotion_recognition_data',
+  HANDLE_PUT_EMOTION_RECOGNITION_DATA: 'handle_put_emotion_recognition_data',
 };
