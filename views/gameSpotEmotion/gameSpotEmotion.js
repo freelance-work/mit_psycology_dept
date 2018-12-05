2e220f200183f36d7ce95e463249d0999754b9d1
Abid Hussain <abidhussain3008@gmail.com>
2018-12-05 23:12:47 +0530

    add gameplay of game 1

--- views/gameSpotEmotion/gameSpotEmotion.js
+++ views/gameSpotEmotion/gameSpotEmotion.js
@@ -1,28 +1,59 @@
-const electron =  require('electron');
+const electron = require('electron');
+const payload = require('../../assets/emotion_recognition/emotion_recognition');
 const { ipcRenderer } = electron;
+const dataSet = { 'data': payload.data.sort(() => Math.random() - 0.5) };
+
 
 const {
-    HANDLE_LANGUAGE_CHANGE,
+  HANDLE_LANGUAGE_CHANGE,
 } = require('../../utils/constants');
 
 $(document).ready(() => {
-  let string =  JSON.parse(window.localStorage.getItem('lang'));
-  console.log(string)
-  $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
-  $('.g1-instructions').html(string.strings.game1.instruction);
-  
+  let idx = 0;
+  let outputPayload = { "data": [] };
+  let startTime = new Date();
+  let endTime = new Date();
+  let string = JSON.parse(window.localStorage.getItem('lang'));
+
+  $('.image-box').css('background-image', 'url(../../assets/emotion_recognition/faces/' + dataSet.data[idx].faceID + '.jpg)');
+
+  $('.emotion-button').click(function () {
+    endTime = new Date();
+    let data = { "faceId": dataSet.data[idx].faceID };
+    let reactionTime = (endTime.getTime() - startTime.getTime()) / 1000;
+    data = {
+      ...data,
+      "reactionTime": reactionTime,
+    }
+    if (this.id == dataSet.data[idx].response) {
+      data = {
+        ...data,
+        "answer": "correct",
+      }
+    } else {
+      data = {
+        ...data,
+        "answer": "incorrect",
+      }
+    }
+    outputPayload.data.push(data)
+    
+    if (idx > 69) {
+      console.log(outputPayload.data);
+      console.log("open modal")
+    } else {
+      setTimeout(() => {
+        $('.image-box').css('background-image', 'url(../../assets/emotion_recognition/faces/' + dataSet.data[++idx].faceID + '.jpg)');
+        startTime = new Date();
+      }, 1000);
+    }
+  });
 });
 
 $(window).on('resize', () => {
-    $('.game-item').css({ 'height': $('.game-item').width() + 'px' });
+  $('.game-item').css({ 'height': $('.game-item').width() + 'px' });
 });
 
 ipcRenderer.on(HANDLE_LANGUAGE_CHANGE, (e, string) => {
-    window.localStorage.setItem('lang', string);
-    $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
-    $('.g1-instructions').html(string.strings.game1.instruction);
-});
-
-$(".start-g1-btn").on('click', () =>{
-    window.location = "gamespotEmotion.html";
+  window.localStorage.setItem('lang', string);
 });
---
+++ views/gameSpotEmotion/instructions.js
@@ -0,0 +1,26 @@
+const electron =  require('electron');
+const { ipcRenderer } = electron;
+
+const {
+    HANDLE_LANGUAGE_CHANGE,
+} = require('../../utils/constants');
+
+$(document).ready(() => {
+  let string = JSON.parse(window.localStorage.getItem('lang'));
+  $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
+  $('.g1-instructions').html(string.strings.game1.instruction);
+});
+
+$(window).on('resize', () => {
+    $('.game-item').css({ 'height': $('.game-item').width() + 'px' });
+});
+
+ipcRenderer.on(HANDLE_LANGUAGE_CHANGE, (e, string) => {
+    window.localStorage.setItem('lang', string);
+    $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
+    $('.g1-instructions').html(string.strings.game1.instruction);
+});
+
+$('.start-g1-btn').on('click', () =>{
+    window.location = 'gamespotEmotion.html';
+});
