29549e935d1fc0762309aa15d23c70f88d5bf5f4
Adarsh <adarshbhalotia2910@gmail.com>
2018-12-05 20:04:54 +0530

    Add key js for images

---
+++ assets/emotion_recognition/emotion_recognition.js
@@ -0,0 +1,307 @@
+
+module.exports = {
+  [
+    {
+      faceID: 'SS_1188',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'SS_0539',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'SS_0302',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'SS_0151',
+      response: 'NEUTRAL',
+      
+    },
+    {
+      faceID: 'SS_0084',
+      response: 'DISGUST'
+    },
+    {
+      faceID: 'SS_0032',
+      response: 'SURPRISE'
+    }
+    {
+      faceID: 'SS_0018',
+      response: 'JOY'
+    },
+    {
+      faceID: 'PB_1383',
+      response: 'SURPRISE'
+    },
+    {
+      faceID: 'PB_0499',
+      response: 'JOY'
+    },
+    {
+      faceID: 'PB_0442',
+      response: 'DISGUST'
+    },
+    {
+      faceID: 'PB_0314',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'PB_0269',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'PB_0144',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'PB_0001',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID: 'PA_1701',
+      response: 'DISGUST'
+    },
+    {
+      faceID: 'PA_1348',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'PB_0967',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'PA_0840',
+      response: 'SURPRISE' 
+    },
+    {
+      faceID: 'PA_0394',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'PB_0112',
+      response: 'JOY'
+    },
+    {
+      faceID: 'PA_0006',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID: 'MR2_1829',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'MR2_1290',
+      response: 'DISGUST'      
+    },
+    {
+      faceID: 'MR2_0896',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'MR2_0580',
+      response: 'SURPRISE'
+    },
+    {
+      faceID: 'MR2_0063',
+      response: 'JOY'
+    },
+    {
+      faceID: 'MR2_0014',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID: 'MR_2767',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'MR_2450',
+      response: 'ANGER',
+      
+    },
+    {
+      faceID: 'MR_1669',
+      response: 'DISGUST',
+      
+    },
+    {
+      faceID: 'MR_0959',
+      response: 'FEAR',
+      
+    },
+    {
+      faceID: 'MR_0619',
+      response: 'SURPRISE',
+      
+    },
+    {
+      faceID: 'MR_0055',
+      response: 'JOY',
+      
+    },
+    {
+      faceID: 'MR_0013',
+      response: 'NEUTRAL',
+      
+    },
+    {
+      faceID: 'KS_4222',
+      response: 'JOY',
+      
+    },
+    {
+      faceID: 'KS_0624',
+      response: 'FEAR',
+      
+    },
+    {
+      faceID: 'KP_1148',
+      response: 'FEAR',
+      
+    },
+    {
+      faceID: 'KP_0991',
+      response: 'SADNESS',
+      
+    },
+    {
+      faceID: 'KP_0760',
+      response: 'ANGER',
+      
+    },
+    {
+      faceID: 'KP_0351',
+      response: 'DISGUST',
+      
+    },
+    {
+      faceID: 'KP_0225',
+      response: 'SURPRISE',
+      
+    },
+    {
+      faceID: 'KP_0082',
+      response: 'NEUTRAL',
+      
+    },
+    {
+      faceID: 'KP_0051',
+      response: 'JOY',
+      
+    },
+    {
+      faceID: 'KA_2396',
+      response: 'SADNESS',
+      
+    },
+    {
+      faceID: 'KA_1616',
+      response: 'ANGER',
+      
+    },
+    {
+      faceID: 'KA_1134',
+      response: 'FEAR',
+      
+    },
+    {
+      faceID: 'KA_0884',
+      response: 'SURPRISE',
+      
+    },
+    {
+      faceID: 'KA_0535',
+      response: 'DISGUST',
+      
+    },
+    {
+      faceID: 'KA_0043',
+      response: 'JOY'
+    },
+    {
+      faceID: 'KA_0003',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID: 'JG_2291',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'JG_1632',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'JG_1465',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'JG_1329',
+      response: 'SURPRISE'
+    },
+    {
+      faceID: 'JG_1226',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID: 'JG_0134',
+      response: 'DISGUST'
+    },
+    {
+      faceID: 'JG_0024',
+      response: 'JOY'
+    },
+    {
+      faceID: 'HW_2478',
+      response: 'SADNESS'
+    },
+    {
+      faceID: 'HW_2219',
+      response: 'FEAR'
+    },
+    {
+      faceID: 'HW_2057',
+      response: 'SURPRISE'
+    },
+    {
+      faceID: 'HW_1612',
+      response: 'ANGER'
+    },
+    {
+      faceID: 'HW_0452',
+      response: 'DISGUST'
+    },
+    {
+      faceID: 'HW_0068',
+      response: 'JOY'
+    },
+    {
+      faceID: 'HW_0006',
+      response: 'NEUTRAL'
+    },
+    {
+      faceID:'KS_3640',
+      response:'ANGER'
+    },
+    {
+      faceID:'KS_2161',
+      response:'NEUTRAL'
+    },
+    {
+      faceID:'KS_2052',
+      response:'SADNESS'
+    },
+    {
+      faceID:'KS_0993',
+      response:'DISGUST'
+    },
+    {
+      faceID:'KS_0252',
+      response:'SURPRISE'
+    },
+    {
+      faceID:'MR2_2086',
+      response:'SADNESS'
+    },  
+
+  ]
+}
