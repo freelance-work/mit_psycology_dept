588ad2eb3ab20d4bdc546dc88f8e0b22e30e8783
Abid Hussain <abidhussain3008@gmail.com>
2018-12-06 21:24:41 +0530

    add GET_EMOTION_RECOGNITION_DATA

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,8 @@
 module.exports = {
   CLEAR_STORAGE: 'clear_storage',
   HANDLE_CLEAR_STORAGE: 'handle_clear_storage',
-
   HANDLE_LANGUAGE_CHANGE: 'handle_language_change',
-  
   PUT_EMOTION_RECOGNITION_DATA: 'put_emotion_recognition_data',
   HANDLE_PUT_EMOTION_RECOGNITION_DATA: 'handle_put_emotion_recognition_data',
+  GET_EMOTION_RECOGNITION_DATA: 'get_emotion_recognition_data',
 };
