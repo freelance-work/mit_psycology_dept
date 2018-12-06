45c47aba5d46a8abed73187bc7e4727cfe36250e
Adarsh <adarshbhalotia2910@gmail.com>
2018-12-06 18:15:26 +0530

    Bug fix

--- views/gameSpotEmotion/gameSpotEmotion.js
+++ views/gameSpotEmotion/gameSpotEmotion.js
@@ -23,7 +23,7 @@ $(document).ready(() => {
     string = JSON.parse(window.localStorage.getItem('lang'));
   } catch(err) { };
 
-  $('.image-box').css('background-image', 'url(../assets/emotion_recognition/faces/' + dataSet.data[idx].faceID + '.jpg)');
+  $('.image-box').css('background-image', 'url(../../assets/emotion_recognition/faces/' + dataSet.data[idx].faceID + '.jpg)');
 
   $('.emotion-button').click(function () {
     $('.image-box').css('background-image', 'none');
@@ -52,7 +52,7 @@ $(document).ready(() => {
       ipcRenderer.send(PUT_EMOTION_RECOGNITION_DATA, outputPayload);
     } else {
       setTimeout(() => {
-        $('.image-box').css('background-image', 'url(../assets/emotion_recognition/faces/' + dataSet.data[idx].faceID + '.jpg)');
+        $('.image-box').css('background-image', 'url(../../assets/emotion_recognition/faces/' + dataSet.data[idx].faceID + '.jpg)');
         $('.emotion-button').prop('disabled', false);
         startTime = new Date();
       }, 500);
