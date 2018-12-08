5e89d3c6db8fd44bdd514204f5b47c22fd4d49f8
Abid Hussain <abidhussain3008@gmail.com>
2018-12-08 21:49:35 +0530

    add go-nogo gameplay

---
+++ views/goNoGo/goNoGo.js
@@ -0,0 +1,71 @@
+const electron = require('electron');
+const { ipcRenderer } = electron;
+const gamePayload = require('../../assets/gonogo');
+const {
+    HANDLE_LANGUAGE_CHANGE,
+    GET_EMOTION_RECOGNITION_DATA
+} = require('../../utils/constants');
+let face;
+let resp = { faceID: '', response: 'noResponse' };
+let setCount = 0;
+let setArr = [{name: 'JOY', obj : gamePayload.joyAnger},{name: 'JOY',obj : gamePayload.joyNeutral},{name: 'NEUTRAL', obj: gamePayload.angerNeutral},{name: 'NEUTRAL'},{name: 'ANGER'},{name:'ANGER'}];
+let payload = { "data": [] }
+
+$(document).ready(() => {
+    startGame(setArr[setCount].obj);
+    $(window).keypress(function (e) {
+        if (e.which === 32) {
+            if (face.response == setArr[set]) {
+                if (resp.response == 'noResponse') {
+                    resp = { ...resp, response: 'correct' }
+                }
+            } else {
+                if (resp.response == 'noResponse') {
+                    resp = { ...resp, response: 'incorrect' }
+                }
+            }
+        }
+    });
+});
+
+$('.continue-btn').on('click', () => {
+    $('.modal-container').hide();
+    let payload = { "data": [] }
+    startGame(setArr[setCount].obj);
+})
+
+startGame = (payloadSet) => {
+    let quad;
+    let random_idx;
+    let count = 0;
+    let timer = setInterval(function () {
+        if (count != 0) {
+            payload.data.push(resp);
+            console.log(payload);
+        }
+
+        $('.quadrant').css({ 'background-image': 'url()' });
+        resp = { faceID: '', response: 'noResponse', set: setCount }
+        if (++count > 30) {
+            setCount++;
+            clearInterval(timer);
+            $('.modal-container').show();
+        }
+        setTimeout(function () {
+            quad = Math.floor(Math.random() * 4) + 1;
+            random_idx = Math.random();
+            if (random_idx <= 0.75) {
+                face = payloadSet[Math.floor(Math.random() * 30)];
+            } else {
+                face = payloadSet[Math.floor(Math.random() * 30) + 30];
+            }
+            resp = {
+                ...resp,
+                faceID: face.faceID,
+                quadrant : quad,
+                emotion : face.response,
+            };
+            $('.quadrant' + quad).css({ 'background-image': 'url(../../assets/faces/' + face.faceID + '.jpg)' });
+        }, 5);
+    }, 50);
+}
---
+++ views/goNoGo/instructions.js
@@ -0,0 +1,26 @@
+const electron =  require('electron');
+const { ipcRenderer } = electron;
+const {
+    HANDLE_LANGUAGE_CHANGE,
+} = require('../../utils/constants');
+
+
+// $(document).ready(() => {
+//   try {
+//     let string = JSON.parse(window.localStorage.getItem('lang'));
+//     $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
+//     $('.g1-instructions').html(string.strings.game1.instruction);
+//     $('#start-btn').text(string.strings.game1.startButtonText);
+//   } catch(err) { };
+// });
+
+// $(window).on('resize', () => {
+//     $('.game-item').css({ 'height': $('.game-item').width() + 'px' });
+// });
+
+// ipcRenderer.on(HANDLE_LANGUAGE_CHANGE, (e, string) => {
+//     window.localStorage.setItem('lang', JSON.stringify(string));
+//     $('.g1-instruction-title').html(string.strings.game1.instructionTitle);
+//     $('.g1-instructions').html(string.strings.game1.instruction);
+//     $('#start-btn').text(string.strings.game1.startButtonText);
+// });
